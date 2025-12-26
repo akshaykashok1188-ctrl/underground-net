6b5f0d0c7f8e42215308c0259888036c33c8a974
apple <apple@macmy.local>
2025-12-26 20:42:31 +0530

    fixed firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // താഴെയുള്ള ഭാഗത്ത് നിങ്ങ‌ളുടെ Firebase കൺസോളിൽ നിന്ന് കിട്ടിയ 
 // സ്വന്തം "firebaseConfig" കോപ്പി ചെയ്ത് പേസ്റ്റ് ചെയ്യുക.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAmqxzfN1aOrlPXLVA629lrC33y4ei7FUQ",
+  authDomain: "underground-net-v2.firebaseapp.com",
+  projectId: "underground-net-v2",
+  storageBucket: "underground-net-v2.firebasestorage.app",
+  messagingSenderId: "1005372859050",
+  appId: "1:1005372859050:web:166e1d7fde1f693aaea6d3"
 };
 
 // Initialize Firebase
